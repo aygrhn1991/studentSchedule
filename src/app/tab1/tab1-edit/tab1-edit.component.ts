0da9f500ebede3af8d25bc07c69fbfc4cbbc41f1
aygrhn1991 <411610370@qq.com>
2021-02-25 14:23:34 +0800

    1

---
+++ src/app/tab1/tab1-edit/tab1-edit.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1-edit',
+  templateUrl: './tab1-edit.component.html',
+  styleUrls: ['./tab1-edit.component.scss'],
+})
+export class Tab1EditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,18 +3,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { Tab1EditComponent } from './tab1-edit/tab1-edit.component';
+import { RouterModule, Routes } from '@angular/router';
 
-import { Tab1PageRoutingModule } from './tab1-routing.module';
+const routes: Routes = [
+  { path: '', component: Tab1Page },
+  { path: 'tab1-edit', component: Tab1EditComponent }
+];
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    RouterModule.forChild(routes),
   ],
-  declarations: [Tab1Page]
+  declarations: [
+    Tab1Page,
+    Tab1EditComponent
+  ]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule { }
